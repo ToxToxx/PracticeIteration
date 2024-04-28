63e67900262286fdda433f8d5bfb363d0d879696
Matvey <sinema3d00@gmail.com>
2024-04-28 21:54:34 +0300

    \

--- Assets/Scripts/RedefinedMover.cs
+++ Assets/Scripts/RedefinedMover.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class RedefinedMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector3 _startPosition = new Vector3(-2.5f, 0, 0);
+    [SerializeField] private Vector3 _endPosition = new Vector3(2.5f, 0, 0);
+    [SerializeField] private int _stepsCount = 100;
+
+    private IEnumerator _enumerator;
+    private float _timer = 0.0f;
+    private float _timeStep;
+
+    private void Start()
     {
-        
+        _enumerator = GetEnumerator();
+        _timeStep = 1.0f / _stepsCount;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(_timer <= Time.time)
+        {
+            _enumerator.MoveNext();
+            _timer = Time.time + _timeStep; 
+        }
+    }
+
+    private IEnumerator GetEnumerator()
+    {
+        return EnumeratePositions(_startPosition, _endPosition, _stepsCount);
+    }
+
+    private IEnumerator EnumeratePositions(Vector3 start, Vector3 end, int steps)
+    {
+        Vector3 distance = start - end;
+        Vector3 stepValue = distance / steps;
+        Vector3 currentStep = stepValue;
+
+        for (int step = 1; step <= steps; step++)
+        {
+            currentStep = stepValue * step;
+            transform.position = start - currentStep;
+            yield return null;
+        }
     }
 }
